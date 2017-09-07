2267a6d7b8106381e17fff495a92a2a1db269c16
msgctcheng <msgctcheng@gmail.com>
2017-09-06 23:20:35 -0400

    added dropdowns to resume and effects, added resume download pdf button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,28 @@
+
+var drop = document.getElementsByClassName("dropdown");
+var i;
+for (i = 0; i < drop.length; i++) {
+        drop[i].onclick = function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        }
+    }
+
+
+
+
+
 function openTab(name, content) {
     var i;
     var tabContent;
     var tabs;
     tabContent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabContent.length; i++) {
+    for (var i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
    
@@ -13,3 +32,6 @@ function openTab(name, content) {
 
 }
 document.getElementById("startTab").click(); 
+
+
+
